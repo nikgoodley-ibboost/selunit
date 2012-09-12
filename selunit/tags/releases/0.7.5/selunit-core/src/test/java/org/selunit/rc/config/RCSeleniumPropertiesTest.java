0bfc2a24caac3a0e9fd38857379a6dfbf15414a4
mboook@gmail.com <mboook@gmail.com@cd90b2e1-6fbb-a060-4294-cbf727b161ca>
2012-09-12 20:23:51 +0000

    [maven-release-plugin]  copy for tag releases/0.7.5

---
+++ selunit/tags/releases/0.7.5/selunit-core/src/test/java/org/selunit/rc/config/RCSeleniumPropertiesTest.java
@@ -0,0 +1,39 @@
+package org.selunit.rc.config;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Test for {@link RCSeleniumProperties}.
+ * 
+ * @author mbok
+ * 
+ */
+public class RCSeleniumPropertiesTest {
+	@Test
+	public void testGettersSetters() {
+		RCSeleniumProperties rc = new RCSeleniumProperties();
+		// Test defaults
+		Assert.assertEquals(4449, rc.getPort());
+		Assert.assertEquals(true, rc.isMultiWindow());
+		Assert.assertEquals(true, rc.isFailFast());
+		Assert.assertNull(rc.getBrowserKey());
+		Assert.assertNull(rc.getUserExtensions());
+
+		// Set
+		rc.setBrowserKey("*firefox");
+		rc.setMultiWindow(false);
+		rc.setPort(5999);
+		rc.setUserExtensions("ext.js");
+		rc.setFailFast(false);
+
+		// Check
+		Assert.assertEquals(5999, rc.getPort());
+		Assert.assertEquals(false, rc.isMultiWindow());
+		Assert.assertEquals(false, rc.isFailFast());
+		Assert.assertEquals("*firefox", rc.getBrowserKey());
+		Assert.assertEquals("ext.js", rc.getUserExtensions());
+
+	}
+}
