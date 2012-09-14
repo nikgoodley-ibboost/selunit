54919d7a75a0ae57cc2ba763d3df72f8c016b953
mboook <mboook@gmail.com>
2012-09-14 18:50:27 +0000

    [maven-release-plugin]  copy for tag releases/0.7.6

---
+++ selunit/tags/releases/0.7.6/selunit-core/src/main/java/org/selunit/webdriver/EvaluationException.java
@@ -0,0 +1,15 @@
+package org.selunit.webdriver;
+
+public class EvaluationException extends TestException {
+
+	private static final long serialVersionUID = -2166991454941907785L;
+
+	public EvaluationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EvaluationException(String message) {
+		super(message);
+	}
+
+}
