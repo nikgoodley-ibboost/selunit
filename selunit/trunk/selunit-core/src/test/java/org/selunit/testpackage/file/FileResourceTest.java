0fe78214c682d78a248a94b39a88f505cc9278bb
mboook <mboook@cd90b2e1-6fbb-a060-4294-cbf727b161ca>
2013-09-10 20:44:51 +0000

    Support for spaces / wrong "+" in suite names

--- selunit/trunk/selunit-core/src/test/java/org/selunit/testpackage/file/FileResourceTest.java
+++ selunit/trunk/selunit-core/src/test/java/org/selunit/testpackage/file/FileResourceTest.java
@@ -24,9 +24,6 @@ import org.junit.Test;
 import org.selunit.job.support.DefaultTestJob;
 import org.selunit.testpackage.TestResource;
 import org.selunit.testpackage.TestResourceAccess;
-import org.selunit.testpackage.file.DirectoryFileAccess;
-import org.selunit.testpackage.file.FileResource;
-
 
 /**
  * Test for {@link FileResource} and {@link DirectoryFileAccess}.
@@ -46,18 +43,16 @@ public class FileResourceTest {
 				"src/test/resources"));
 		DefaultTestJob job = new DefaultTestJob();
 		Assert.assertNotNull(p.getResource(job, "SuiteSimple.html"));
+		Assert.assertNotNull(p.getResource(job, "Copy of SuiteSimple.html"));
 		Assert.assertNull(p.getResource(job, "EOF.html"));
-		Assert.assertNotNull(p.getResource(job,
-				"testcases/GoogleSearch1.html"));
-		Assert.assertNull(p.getResource(job,
-				"testcases/GoogleSearch12345.html"));
-		Assert.assertNotNull(p.getResource(job,
-				"/testcases/GoogleSearch1.html"));
-		Assert.assertEquals(
-				p.getResource(job, "testcases/GoogleSearch1.html")
-						.getLength(),
-				p.getResource(job, "/testcases/GoogleSearch1.html")
-						.getLength());
+		Assert.assertNotNull(p.getResource(job, "testcases/GoogleSearch1.html"));
+		Assert.assertNull(p
+				.getResource(job, "testcases/GoogleSearch12345.html"));
+		Assert.assertNotNull(p
+				.getResource(job, "/testcases/GoogleSearch1.html"));
+		Assert.assertEquals(p.getResource(job, "testcases/GoogleSearch1.html")
+				.getLength(),
+				p.getResource(job, "/testcases/GoogleSearch1.html").getLength());
 	}
 
 	/**
@@ -69,11 +64,11 @@ public class FileResourceTest {
 	public void testResources() throws Exception {
 		DirectoryFileAccess p = new DirectoryFileAccess(new File(
 				"src/test/resources"));
-		TestResource r = p.getResource(new DefaultTestJob(),
-				"SuiteSimple.html");
+		TestResource r = p
+				.getResource(new DefaultTestJob(), "SuiteSimple.html");
 		long len;
-		Assert.assertEquals((len = new File(
-				"src/test/resources/SuiteSimple.html").length()), r.getLength());
+		Assert.assertEquals(len = new File(
+				"src/test/resources/SuiteSimple.html").length(), r.getLength());
 		Assert.assertEquals("SuiteSimple.html", r.getName());
 		byte[] buffer = new byte[4096];
 		InputStream is = r.getContent();
