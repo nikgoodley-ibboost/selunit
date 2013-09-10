c9272ebad101f64067c8cac30001d70eeb7f84de
mboook@gmail.com <mboook@gmail.com@cd90b2e1-6fbb-a060-4294-cbf727b161ca>
2013-09-10 20:51:20 +0000

    [maven-release-plugin]  copy for tag releases/0.7.15

---
+++ selunit/tags/releases/0.7.15/selunit-core/src/test/java/org/selunit/rc/testpackage/ResourcePackageLocatorTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2011 selunit.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.selunit.rc.testpackage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openqa.jetty.http.HttpContext;
+import org.openqa.jetty.util.IO;
+import org.openqa.jetty.util.Resource;
+import org.selunit.job.support.DefaultTestJob;
+import org.selunit.testpackage.file.DirectoryFileAccess;
+
+/**
+ * Test for {@link TestResourceLocator}.
+ * 
+ * @author mbok
+ * 
+ */
+public class ResourcePackageLocatorTest {
+	/**
+	 * Tests {@link Resource} methods via {@link TestResourceLocator}.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testLocator() throws Exception {
+		TestResourceLocator locator = new TestResourceLocator(
+				new DefaultTestJob(), new DirectoryFileAccess(new File(
+						"src/test/resources")), "/myctx");
+		Assert.assertFalse(locator.getResource(new HttpContext(), "missing")
+				.exists());
+		Assert.assertFalse(locator.getResource(new HttpContext(),
+				"/other-ctx/SuiteSimple.html").exists());
+		Resource r = locator.getResource(new HttpContext(),
+				"/myctx/SuiteSimple.html");
+		Assert.assertTrue(r.exists());
+		Assert.assertTrue(locator.getResource(new HttpContext(),
+				"/myctx/Copy of SuiteSimple.html").exists());
+		long len;
+		Assert.assertEquals(len = new File(
+				"src/test/resources/SuiteSimple.html").length(), r.length());
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		IO.copy(r.getInputStream(), buffer);
+		Assert.assertEquals(len, buffer.size());
+	}
+}
